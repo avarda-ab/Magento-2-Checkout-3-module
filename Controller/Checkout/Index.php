1a198d9aec2ce89a4e159e748bc095ed503dd3b3
Juhani Haapala <juhani@codaone.fi>
2021-08-19 10:03:44 +0300

    Add backend check for state and do reinit if needed

--- Controller/Checkout/Index.php
+++ Controller/Checkout/Index.php
@@ -6,63 +6,63 @@
 namespace Avarda\Checkout3\Controller\Checkout;
 
 use Avarda\Checkout3\Controller\AbstractCheckout;
+use Avarda\Checkout3\Gateway\Config\Config;
+use Avarda\Checkout3\Helper\PaymentData;
+use Avarda\Checkout3\Helper\PurchaseState;
+use Avarda\Checkout3\Model\QuotePaymentManagement;
+use Exception;
+use Magento\Checkout\Helper\Data;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Psr\Log\LoggerInterface;
 
 class Index extends AbstractCheckout
 {
-    /**
-     * @var \Magento\Framework\View\Result\PageFactory
-     */
+    /** @var PageFactory */
     protected $resultPageFactory;
 
-    /**
-     * @var \Magento\Checkout\Helper\Data
-     */
+    /** @var Data */
     protected $checkoutHelper;
 
-    /**
-     * @var \Magento\Customer\Model\Session
-     */
+    /** @var CustomerSession */
     protected $customerSession;
 
-    /**
-     * @var \Magento\Checkout\Model\Session
-     */
+    /** @var CheckoutSession */
     protected $checkoutSession;
 
-    /**
-     * @var \Magento\Quote\Api\CartRepositoryInterface
-     */
+    /** @var CartRepositoryInterface */
     protected $quoteRepository;
 
-    /**
-     * Request instance
-     *
-     * @var \Magento\Framework\App\RequestInterface
-     */
+    /** @var RequestInterface */
     protected $request;
 
-    /**
-     * Index constructor.
-     *
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param \Avarda\Checkout3\Gateway\Config\Config $config
-     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
-     * @param \Magento\Checkout\Helper\Data $checkoutHelper
-     * @param \Magento\Checkout\Model\Session $checkoutSession
-     * @param \Magento\Customer\Model\Session $customerSession
-     * @param \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
-     */
+    /** @var QuotePaymentManagement */
+    protected $quotePaymentManagement;
+
+    /** @var PurchaseState */
+    protected $purchaseState;
+
+    /** @var PaymentData */
+    protected $paymentData;
+
     public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Psr\Log\LoggerInterface $logger,
-        \Avarda\Checkout3\Gateway\Config\Config $config,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \Magento\Checkout\Helper\Data $checkoutHelper,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
-        \Magento\Framework\App\RequestInterface $request
+        Context $context,
+        LoggerInterface $logger,
+        Config $config,
+        PageFactory $resultPageFactory,
+        Data $checkoutHelper,
+        CheckoutSession $checkoutSession,
+        CustomerSession $customerSession,
+        CartRepositoryInterface $quoteRepository,
+        RequestInterface $request,
+        QuotePaymentManagement $quotePaymentManagement,
+        PurchaseState $purchaseState,
+        PaymentData $paymentData
     ) {
         parent::__construct($context, $logger, $config);
         $this->resultPageFactory = $resultPageFactory;
@@ -71,10 +71,13 @@ class Index extends AbstractCheckout
         $this->customerSession = $customerSession;
         $this->quoteRepository = $quoteRepository;
         $this->request = $request;
+        $this->quotePaymentManagement = $quotePaymentManagement;
+        $this->purchaseState = $purchaseState;
+        $this->paymentData = $paymentData;
     }
 
     /**
-     * @return \Magento\Framework\Controller\ResultInterface
+     * @return ResultInterface
      */
     public function execute()
     {
@@ -119,7 +122,7 @@ class Index extends AbstractCheckout
         $paymentCode = '';
         try {
             $paymentCode = $quote->getPayment()->getMethod();
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             // pass
         }
         // Remove payment method if it's not avarda payment already
@@ -130,6 +133,12 @@ class Index extends AbstractCheckout
         if ($needsSave) {
             // for unknown reason shipping method is not saved with repository save
             $quote->save();
+        }
+
+        $state = $this->paymentData->getState($quote->getPayment());
+        if ($this->purchaseState->isComplete($state) || $this->purchaseState->isDead($state)) {
+            $quote->collectTotals();
+            $this->quotePaymentManagement->initializePurchase($quote);
         }
 
         $this->customerSession->regenerateId();
