b46b6df46dd4cc65de1dffec13d6723bccd7c35c
Juhani Haapala <juhani@codaone.fi>
2023-02-23 14:10:54 +0200

    Add validation that purchaseId matches purhcase id which is saved to quote

--- Plugin/Checkout/PlaceOrderPluginAbstract.php
+++ Plugin/Checkout/PlaceOrderPluginAbstract.php
@@ -8,8 +8,8 @@ namespace Avarda\Checkout3\Plugin\Checkout;
 
 use Avarda\Checkout3\Api\AvardaOrderRepositoryInterface;
 use Avarda\Checkout3\Api\Data\PaymentDetailsInterface;
-use Avarda\Checkout3\Model\AvardaOrderFactory;
 use Magento\Framework\Exception\AlreadyExistsException;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Quote\Api\Data\AddressInterface;
 use Magento\Quote\Api\Data\CartInterface;
 use Magento\Quote\Model\Quote;
@@ -57,6 +57,7 @@ abstract class PlaceOrderPluginAbstract
         $shippingAddress->setCity($additionalData['deliveryAddress']['city']);
         $shippingAddress->setPostcode($additionalData['deliveryAddress']['zip']);
         $shippingAddress->setCountryId($additionalData['deliveryAddress']['country']);
+
         // @todo fix this when phone number is available
         // We don't have customer phone number, so we add dummy here
         // After order is paid it will be updated by status update
@@ -88,6 +89,7 @@ abstract class PlaceOrderPluginAbstract
         $billingAddress->setCity($additionalData['invoicingAddress']['city']);
         $billingAddress->setPostcode($additionalData['invoicingAddress']['zip']);
         $billingAddress->setCountryId($additionalData['invoicingAddress']['country']);
+
         // @todo fix this when phone number is available
         // We don't have customer phone number, so we add dummy here
         // After order is paid it will be updated by status update
@@ -118,5 +120,22 @@ abstract class PlaceOrderPluginAbstract
             $email = $additionalData['email'];
         }
         return $email;
+    }
+
+    /**
+     * Validate that frontend purchaseId matches purchaseId saved in quote
+     *
+     * @param $quote Quote|CartInterface
+     * @param $data array
+     * @return true
+     * @throws LocalizedException
+     */
+    public function validatePurchase($quote, $data)
+    {
+        $purchaseId = $quote->getPayment()->getAdditionalInformation(PaymentDetailsInterface::PURCHASE_DATA)['purchaseId'];
+        if ($purchaseId != $data['purchaseId']) {
+            throw new LocalizedException(__('PurchaseId does not match, please try again'));
+        }
+        return true;
     }
 }
