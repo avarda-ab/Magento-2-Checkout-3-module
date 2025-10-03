0a2880c55e1ee1c69a3a78067445ed2867085b01
Pete Haapala <peter@codaone.fi>
2025-10-03 11:52:48 +0300

    Add onepage redirect check to address update plugin

--- Plugin/Controller/Checkout/UpdateAvardaDeliveryAddress.php
+++ Plugin/Controller/Checkout/UpdateAvardaDeliveryAddress.php
@@ -16,30 +16,36 @@ class UpdateAvardaDeliveryAddress
     protected Session $checkoutSession;
     protected CommandPoolInterface $commandPool;
     protected PaymentDataObjectFactoryInterface $paymentDataObjectFactory;
+    protected RequestInterface $request;
 
     public function __construct(
         Session $checkoutSession,
         CommandPoolInterface $commandPool,
         PaymentDataObjectFactoryInterface $paymentDataObjectFactory,
+        RequestInterface $request
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->commandPool = $commandPool;
         $this->paymentDataObjectFactory = $paymentDataObjectFactory;
+        $this->request = $request;
     }
 
 
     public function afterExecute($subject, $result)
     {
-        $quote = $this->checkoutSession->getQuote();
-        $payment = $quote->getPayment();
-        $argument = [
-            'payment' => $this->paymentDataObjectFactory->create($payment),
-        ];
+        // Update Avarda order delivery address if coming from Magento onepage checkout
+        if ($this->request->getParam('fromCheckout')) {
+            $quote = $this->checkoutSession->getQuote();
+            $payment = $quote->getPayment();
+            $argument = [
+                'payment' => $this->paymentDataObjectFactory->create($payment),
+            ];
 
-        try {
-            $this->commandPool->get('update_delivery_address')->execute($argument);
-        } catch (Exception $e) {
-            // Update fails if avarda order has timed out
+            try {
+                $this->commandPool->get('update_delivery_address')->execute($argument);
+            } catch (Exception $e) {
+                // Update fails if avarda order has timed out
+            }
         }
 
         return $result;
